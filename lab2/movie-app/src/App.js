5f4bb0fae8114ad8c1b21a2e983cac1d0742c901
jamween <jamielukeconcannon@gmail.com>
2024-10-04 13:35:24 +0000

    created components and installed bootstrap

--- lab2/movie-app/src/App.js
+++ lab2/movie-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
